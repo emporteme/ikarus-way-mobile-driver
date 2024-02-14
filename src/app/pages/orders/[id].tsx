bbf453833662a54317cad73525ee8b6f71f3fc49
Magzhan <magzhankarataev02@gmail.com>
2024-02-14 10:07:14 +0600

    Order details styles were created

--- src/app/pages/orders/[id].tsx
+++ src/app/pages/orders/[id].tsx
@@ -1,8 +1,9 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams, Stack } from 'expo-router';
 import { orders } from '@/components/orders/list/data'
 import { OrderType } from '@/types'
+import styles from '@/style/orderDetails.style';
 
 const OrderDetail = () => {
     const { id } = useLocalSearchParams();
@@ -13,10 +14,26 @@ const OrderDetail = () => {
                 options={{
                     headerShown: true,
                     title: `Order #${id}`,
-                    headerTitleAlign: 'center'
+                    headerTitleAlign: 'center',
+                    headerRight: () => (
+
+                        // Maybe I need to use router.back(), but for now this logic fits well.
+                        // router.push('/pages/services')}
+                        <View style={styles.status}>
+                            <Text style={styles.statusText}>Edit</Text>
+                        </View>
+                    ),
                 }}
             />
             <Text style={{ marginHorizontal: 'auto' }}>{id} USD</Text>
+            <ScrollView>
+                <View>
+                    <Text>Pickup</Text>
+                    <View></View>
+                    <View></View>
+                    <View></View>
+                </View>
+            </ScrollView>
         </SafeAreaView >
     );
 }
---
+++ src/style/orderDetails.style.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+import { FONT, COLORS, SIZES } from "@/constants";
+
+const styles = StyleSheet.create({
+    status: {
+        backgroundColor: COLORS.primary,
+        paddingVertical: 4,
+        paddingHorizontal: 10,
+        borderRadius: 8
+    },
+    statusText: {
+        color: COLORS.white,
+        fontSize: SIZES.semiMedium,
+        fontFamily: FONT.medium,
+    },
+})
+
+export default styles
