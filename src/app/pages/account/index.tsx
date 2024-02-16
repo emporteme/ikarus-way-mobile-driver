31aa8cec5de482f6cdd1a71590b5a6f8dc14a25f
Magzhan <magzhankarataev02@gmail.com>
2024-02-16 10:17:07 +0600

    account page was created

---
+++ src/app/pages/account/index.tsx
@@ -0,0 +1,22 @@
+// Main imports
+import React from 'react';
+import { useWindowDimensions, Text, View, } from "react-native";
+import { Link } from "expo-router";
+// Styles imports
+import styles from '@/style/orders.style';
+import { FONT } from '@/constants';
+
+
+const AccountPage = () => {
+    return (
+        <>
+            <View>
+                <Text>
+                    YO
+                </Text>
+            </View>
+        </>
+    );
+}
+
+export default AccountPage;
