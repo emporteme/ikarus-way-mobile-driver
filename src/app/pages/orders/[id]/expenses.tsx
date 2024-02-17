1f330838594a5b192a223445877c8a7be66b6324
Magzhan <magzhankarataev02@gmail.com>
2024-02-17 12:21:32 +0600

    Expenses creaded

---
+++ src/app/pages/orders/[id]/expenses.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Expenses: React.FC = () => {
+    return (
+        <View>
+            <Text>expenses</Text>
+        </View>
+    )
+}
+
+export default Expenses
