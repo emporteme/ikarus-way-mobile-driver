110b4e5302f890275e5dd52afd1666e0037f075f
Magzhan <magzhankarataev02@gmail.com>
2024-02-20 11:10:07 +0600

    Android 21

---
+++ src/app/pages/messages/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DetailMessage: React.FC<{}> = () => {
+    return (
+        <View>
+            <Text>DetailMessage</Text>
+        </View>
+    )
+}
+
+export default DetailMessage
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ import OrderList from "./pages/orders/list/OrderList";
 import OrderCard from "./pages/orders/card/OrderCard";
 import IotList from "./pages/iot/list/IotList";
 import IotCard from "./pages/iot/card/IotCard";
+import Empty from "./pages/messages/empty/Empty";
 
 // Core Components
 import OuterDropdown from "./core/dropdowns/OuterDropdown";
@@ -10,16 +11,15 @@ import InnerDropdown from "./core/dropdowns/InnerDropdown";
 import FileInput from "./core/FileInput";
 
 export {
-
     // Page Screen Components
     OrderList,
     OrderCard,
     IotList,
     IotCard,
+    Empty,
 
     // Core Components
     OuterDropdown,
     InnerDropdown,
     FileInput,
-    
 };
