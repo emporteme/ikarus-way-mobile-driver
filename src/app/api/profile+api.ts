5879e4566cdebd2136a1ec5b7711113a25fcd2a0
Magzhan <magzhankarataev02@gmail.com>
2024-02-22 17:45:21 +0600

    Android 17

--- src/app/api/profile+api.ts
+++ src/app/api/profile+api.ts
@@ -1,19 +1,14 @@
 import { ExpoRequest, ExpoResponse } from 'expo-router/server';
 
 export async function GET(request: ExpoRequest) {
+    const jwt = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjMsInN1YiI6IkNBUlJJRVJzdXBlcmFkbWluQHByb21ldGVvLmlvIiwiaWF0IjoxNzA4NjAwNTc3LCJleHAiOjE3MDg2MDQxNzcsInRva2VuX3R5cGUiOiJqd3QifQ.u7WVObeAFgUoHZ25uCwg7JYCJCyZLf5O970o7ipq8q4'
     try {
         const url = 'https://app-test.prometeochain.io/api/v1/users/profile';
-        // const body = await request.json();
-        // const credentials = {
-        //     email: body.email,
-        //     password: body.password
-        // };
-
         const response = await fetch(url, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjMsInN1YiI6IkNBUlJJRVJzdXBlcmFkbWluQHByb21ldGVvLmlvIiwiaWF0IjoxNzA4NTg3OTI2LCJleHAiOjE3MDg1OTE1MjYsInRva2VuX3R5cGUiOiJqd3QifQ.Lg_XH6vtF8VxdXTx4iwdXmhPJmnDg1drK8jqFaACPfA'
+                'Authorization': 'Bearer ' + jwt
             },
             // body: JSON.stringify(credentials)
         });
