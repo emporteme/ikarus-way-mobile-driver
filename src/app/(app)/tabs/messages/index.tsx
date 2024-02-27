4658b6abd6f75f3ae3370a2b48e6aae853ab0b58
Magzhan <magzhankarataev02@gmail.com>
2024-02-27 09:38:09 +0600

    messages updated

--- src/app/(app)/tabs/messages/index.tsx
+++ src/app/(app)/tabs/messages/index.tsx
@@ -143,15 +143,6 @@ const MessagesPage: React.FC = () => {
                     style={styles.tabbar}
                 />}
             />
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text
-                    onPress={() => {
-                        // The `app/(app)/_layout.tsx` will redirect to the sign-in screen.
-                        signOut();
-                    }}>
-                    Sign Out
-                </Text>
-            </View>
         </>
     );
 }
