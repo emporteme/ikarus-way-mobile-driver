59c855ef9ab57c10c1d97c802bc20fd9e923aa9b
Magzhan <magzhankarataev02@gmail.com>
2024-02-27 10:05:48 +0600

    EmptyOrders

---
+++ src/components/pages/orders/empty/Empty.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './empty.style'
+
+const EmptyOrders: React.FC = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.textBlock}>
+                <Text>No orders found.</Text>
+            </View>
+        </View>
+    )
+}
+
+export default EmptyOrders
