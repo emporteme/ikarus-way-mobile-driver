fc6ca6b32d4477e1686d68c23244f913b623e6ae
Magzhan <magzhankarataev02@gmail.com>
2024-03-07 14:48:05 +0600

    onboarding 1 fix

--- src/app/(app)/onboarding/index.tsx
+++ src/app/(app)/onboarding/index.tsx
@@ -1,54 +1,58 @@
-// Import React hooks and React Native components
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import {
     View,
     Text,
     Image,
     ImageBackground,
-    Pressable
+    Pressable,
+    ActivityIndicator
 } from 'react-native';
-
-// Import router for navigation
 import { Link } from 'expo-router';
-
-// Import styles
 import styles from '@/styles/onboarding.style';
 import { images, icons } from '@/constants';
 
-
-// Main component
 const OnboardingPage = () => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+
     return (
         <ImageBackground
             source={images.onboarding1}
             style={styles.backgroundImage}
+            onLoad={() => setImageLoaded(true)} // Set imageLoaded to true when image is loaded
         >
-            <View style={styles.pageContent}>
-                <Image
-                    source={images.logo_light}
-                    style={styles.logo}
-                />
-                <View style={styles.footer}>
-                    <Text style={styles.title}>Efficient Logistics at Your Fingertips</Text>
-                    <Text style={styles.description}>Watch your delivery journey on the map in real-time, from the warehouse to your doorstep</Text>
-                    <View style={styles.buttonRow}>
-                        <View style={styles.progress}>
-                            <View style={styles.active} />
-                            <View style={styles.inActive} />
-                            <View style={styles.inActive} />
+            {!imageLoaded && (
+                <View>
+                    <ActivityIndicator size="large" color="#0000ff" />
+                </View>
+            )}
+            {imageLoaded && (
+                <View style={styles.pageContent}>
+                    <Image
+                        source={images.logo_light}
+                        style={styles.logo}
+                    />
+                    <View style={styles.footer}>
+                        <Text style={styles.title}>Efficient Logistics at Your Fingertips</Text>
+                        <Text style={styles.description}>Watch your delivery journey on the map in real-time, from the warehouse to your doorstep</Text>
+                        <View style={styles.buttonRow}>
+                            <View style={styles.progress}>
+                                <View style={styles.active} />
+                                <View style={styles.inActive} />
+                                <View style={styles.inActive} />
+                            </View>
+                            <Link href={'/onboarding/second'} asChild>
+                                <Pressable style={styles.button}>
+                                    <Text style={styles.buttonText}>Next</Text>
+                                    <Image
+                                        source={icons.arrow}
+                                        style={styles.icon}
+                                    />
+                                </Pressable>
+                            </Link>
                         </View>
-                        <Link href={'/onboarding/second'} asChild>
-                            <Pressable style={styles.button}>
-                                <Text style={styles.buttonText}>Next</Text>
-                                <Image
-                                    source={icons.arrow}
-                                    style={styles.icon}
-                                />
-                            </Pressable>
-                        </Link>
                     </View>
                 </View>
-            </View>
+            )}
         </ImageBackground>
     );
 }
