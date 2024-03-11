70c09bf51168a711e43929f9dd66e2848b9686cb
Magzhan <magzhankarataev02@gmail.com>
2024-03-11 12:05:05 +0600

    File sending and opening done in react native gifted chats

--- src/app/(app)/(tabs)/messages/[id].tsx
+++ src/app/(app)/(tabs)/messages/[id].tsx
@@ -1,333 +1,158 @@
-import React, { useState, useCallback, useEffect, useRef, memo } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Image, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Platform } from 'react-native';
 import { useLocalSearchParams, Stack } from 'expo-router';
-import {
-    GiftedChat, Bubble, InputToolbar, Send, SystemMessage, IMessage,
-} from 'react-native-gifted-chat';
+import { GiftedChat, IMessage, User, MessageText } from 'react-native-gifted-chat';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Ionicons, FontAwesome } from '@expo/vector-icons';
-import { InChatFileTransfer } from '@/components';
-import { COLORS, FONT } from '@/constants';
-import messageData from '@/api/chat.json'
-// import * as DocumentPicker from 'react-native-document-picker'
+import { COLORS } from '@/constants';
 import * as DocumentPicker from 'expo-document-picker';
-import { useSession } from '@/components/core/Context';
+import * as FileSystem from 'expo-file-system';
+import * as IntentLauncher from 'expo-intent-launcher';
 
-interface File extends IMessage {
-    url?: string;
-}
+const pickSomething = async (handleFilePicked) => {
+    try {
+        const docRes = await DocumentPicker.getDocumentAsync({
+            type: "*/*",
+            multiple: true, // Allow selecting multiple files
+        });
+
+        if (!docRes.canceled) {
+            const assets = docRes.assets;
+            if (assets) {
+                assets.forEach(async (file) => {
+                    const fileData = {
+                        uri: file.uri,
+                        type: file.mimeType,
+                        name: file.name,
+                        size: file.size,
+                    };
+
+                    // Copy the file to a public directory
+                    const publicDirectory = `${FileSystem.documentDirectory}public/`;
+                    await FileSystem.makeDirectoryAsync(publicDirectory, { intermediates: true });
+                    const publicFile = `${publicDirectory}${file.name}`;
+                    await FileSystem.copyAsync({ from: file.uri, to: publicFile });
+
+                    // Update the file URI to use the public file
+                    fileData.uri = publicFile;
+
+                    handleFilePicked(fileData);
+                });
+            }
+        }
+    } catch (error) {
+        console.log("Error while selecting files: ", error);
+    }
+};
+
+const FileSendingButton = ({ onFilePicked }) => (
+    <TouchableOpacity onPress={() => pickSomething(onFilePicked)} style={styles.fileSendingButton}>
+        <Text style={styles.fileSendingButtonText}>Send File</Text>
+    </TouchableOpacity>
+);
 
 const Page = () => {
-    // WEBSOCKET
-
-    const ws = useRef(null)
-    const { jwtToken } = useSession();
-    const companyid = '3'
-    const orderID = '1'
-    useEffect(() => {
-        console.log('Initial WebSocket connection started');
-
-        ws.current = new WebSocket(`wss://support-test.prometeochain.io/v1/messenger/ws?token=${jwtToken}&companyid=${companyid}&order_id=${orderID}`);
-        ws.current.onopen = () => {
-            console.log('WebSocket connected');
-        };
-
-        ws.current.onmessage = (e) => {
-            console.log('Message received:', e.data);
-            // Handle incoming messages here
-        };
-
-        ws.current.onerror = (error) => {
-            console.error('WebSocket error:', error);
-        };
-
-        ws.current.onclose = () => {
-            console.log('WebSocket closed');
-            // Handle WebSocket close event, you might want to reconnect here
-        };
-
-        // Clean up the WebSocket connection when component unmounts
-        return () => {
-            ws.current.close();
-        };
-    }, []);
-
-    // Memoize components
-    const SystemMessageMemoized = memo(SystemMessage);
-    const BubbleMemoized = memo(Bubble);
-
-    // declare 4 states
-    const [isAttachImage, setIsAttachImage] = useState(false);
-    const [isAttachFile, setIsAttachFile] = useState(false);
-    const [imagePath, setImagePath] = useState('');
-    const [filePath, setFilePath] = useState('');
-
-    // add a function attach file using DocumentPicker.pick
-
-    const _pickDocument = async () => {
-        try {
-            const result = await DocumentPicker.getDocumentAsync({
-                type: '*/*', // You can specify the file types you want to allow, e.g., 'application/pdf', 'image/*', etc.
-            });
-            // Uncomment when will be API
-            // if (result.type === 'success') {
-            //     onSelectFile(result.uri);
-            // } else {
-            //     onSelectFile(null); // User canceled file selection
-            // }
-        } catch (error) {
-            console.error('Error picking file:', error);
-        }
-    };
-
-    const [messages, setMessages] = useState<IMessage[]>([]);
-    const [text, setText] = useState('');
     const insets = useSafeAreaInsets();
-
-    useEffect(() => {
-        setMessages([
-            ...messageData.map((message) => {
-                return {
-                    _id: message.id,
-                    text: message.msg,
-                    createdAt: new Date(message.date),
-                    user: {
-                        _id: message.from,
-                        name: message.from ? 'You' : 'Bob',
-                    },
-                    // Add any other properties you need
-                    // For example, if you want to display an image
-                    // image: message.img, // Assuming you have 'img' property for images
-                    // For files, you might have a property like 'fileUrl'
-                    // fileUrl: message.fileUrl // Add this if you have a 'fileUrl' property in your JSON
-                };
-            }),
-            {
-                _id: 0,
-                system: true,
-                text: 'All your base are belong to us',
-                createdAt: new Date(),
-                user: {
-                    _id: 0,
-                    name: 'Bot',
-                },
-            },
-        ]);
-    }, []);
-
-    useEffect(() => {
-        ws.current.onmessage = e => {
-            const response = JSON.parse(e.data)
-            console.log("onmessage=>", JSON.stringify(response))
-            var sentMessages = [{
-                _id: response.id,
-                text: response.msg,
-                createdAt: new Date(response.createdAt * 1000),
-                user: {
-                    _id: response.from,
-                    name: response.from ? 'You' : 'Bob',
-                },
-            }]
-            setMessages((previousMessages: any[]) => GiftedChat.append(previousMessages, sentMessages))
-        }
-    }, [])
-
-    const onSend = useCallback((messages = []) => {
-        let obj = {
-            "senderId": 'You',
-            "receiverId": "Bob",
-            "message": messages[0].text,
-            "action": "message"
-        }
-        ws.current.send(JSON.stringify(obj))
-        setMessages((previousMessages: any[]) => GiftedChat.append(previousMessages, messages));
-    }, []);
-
-    const renderInputToolbar = (props: any) => {
-        return (
-            <InputToolbar
-                {...props}
-                containerStyle={{ backgroundColor: COLORS.white }}
-                renderActions={() => (
-                    <TouchableOpacity onPress={_pickDocument}>
-                        <View style={{ height: 44, justifyContent: 'center', alignItems: 'center', left: 5 }}>
-                            <Ionicons name="add" color={COLORS.primary} size={28} />
-                        </View>
-                    </TouchableOpacity>
-                )}
-            />
-        );
-    };
-
-
     const { id } = useLocalSearchParams();
 
-    const scrollToBottomComponent = () => {
-        return <FontAwesome name="angle-double-down" size={22} color="#333" />;
+    const [messages, setMessages] = useState<IMessage[]>([]);
+    const [user, setUser] = useState<User>({
+        _id: 1,
+        avatar: 'https://placeimg.com/140/140/any',
+    });
+
+    const onSend = (newMessages: IMessage[]) => {
+        setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessages));
     };
 
-    const renderChatFooter = useCallback(() => {
-        if (imagePath) {
+    const handleFilePicked = async (file) => {
+        try {
+            const fileUri = file.uri;
+            const fileType = file.type;
+            const fileName = file.name;
+            const fileSize = file.size;
+
+            // Create a new message with the file
+            const newMessage: IMessage = {
+                _id: Math.random().toString(36).substring(7),
+                createdAt: new Date(),
+                user,
+                fileUri, // Set the file URI as the fileUri property
+                text: `${fileName} (${fileSize} bytes)`, // Optional text description
+            };
+
+            // Append the new message to the existing messages array
+            setMessages((prevMessages) => GiftedChat.append(prevMessages, [newMessage]));
+        } catch (error) {
+            console.error('Error handling file:', error);
+        }
+    };
+
+    const handleFileOpening = async (fileUri) => {
+        try {
+            const fileExtension = fileUri.split('.').pop().toLowerCase();
+            const supportedExtensions = ['pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'jpg', 'mp3', 'mp4'];
+
+            if (supportedExtensions.includes(fileExtension)) {
+                if (Platform.OS === 'android') {
+                    const contentUri = await FileSystem.getContentUriAsync(fileUri);
+
+                    await IntentLauncher.startActivityAsync('android.intent.action.VIEW', {
+                        data: contentUri,
+                        flags: 1,
+                    });
+                } else {
+                    await FileSystem.openDocumentAsync(fileUri);
+                }
+            } else {
+                console.warn(`Unsupported file extension: ${fileExtension}`);
+            }
+        } catch (error) {
+            console.error('Error opening file:', error);
+        }
+    };
+
+    const renderMessageComponent = (props) => {
+        const { currentMessage } = props;
+
+        if (currentMessage && currentMessage.fileUri) {
             return (
-                <View>
-                    <Image source={{ uri: imagePath }} style={{ height: 75, width: 75 }} />
-                    <TouchableOpacity
-                        onPress={() => setImagePath('')}
-                    // style={styles.buttonFooterChatImg}
-                    >
-                        <Text>X</Text>
-                    </TouchableOpacity>
-                </View>
+                <TouchableOpacity onPress={() => handleFileOpening(currentMessage.fileUri)}>
+                    
+                    <MessageText {...props} />
+                </TouchableOpacity>
             );
         }
-        if (filePath) {
-            return (
-                <View>
-                    <InChatFileTransfer
-                        filePath={filePath}
-                    />
-                    <TouchableOpacity
-                        onPress={() => setFilePath('')}
-                    // style={styles.buttonFooterChat}
-                    >
-                        <Text>X</Text>
-                    </TouchableOpacity>
-                </View>
-            );
-        }
-        return null;
-    }, [filePath, imagePath]);
 
-
+        return <MessageText {...props} />;
+    };
 
     return (
         <SafeAreaView style={{ flex: 1, marginBottom: insets.bottom, backgroundColor: COLORS.background }}>
             <Stack.Screen options={{ title: 'Chat ID: ' + id }} />
-            <>
-                <View style={{ flex: 1 }}>
-                    <GiftedChat
-                        messages={messages}
-                        onSend={(messages: any) => onSend(messages)}
-                        onInputTextChanged={setText}
-                        user={{
-                            _id: 1,
-                        }}
-                        renderSystemMessage={(props) => (
-                            <SystemMessageMemoized {...props} textStyle={{ color: COLORS.gray }} />
-                        )}
-                        bottomOffset={insets.bottom}
-                        renderAvatar={null}
-                        maxComposerHeight={100}
-                        textInputProps={styles.composer}
-                        renderBubble={(props) => {
-                            return (
-                                <BubbleMemoized
-                                    {...props}
-                                    textStyle={{
-                                        left: {
-                                            color: COLORS.dark,
-                                            fontFamily: FONT.medium,
-                                            fontSize: 14,
-                                            lineHeight: 21,
-                                        },
-                                        right: {
-                                            color: COLORS.white,
-                                            fontFamily: FONT.medium,
-                                            fontSize: 14,
-                                            lineHeight: 21,
-                                        },
-                                    }}
-                                    wrapperStyle={{
-                                        left: {
-                                            backgroundColor: COLORS.secondary,
-                                            padding: 10,
-                                            borderRadius: 16
-                                        },
-                                        right: {
-                                            backgroundColor: COLORS.dark,
-                                            padding: 10,
-                                            borderRadius: 16
-                                        },
-                                    }}
-                                />
-                                // <Bubble
-                                //     {...props}
-                                //     textStyle={{
-                                //         left: {
-                                //             color: COLORS.dark,
-                                //             fontFamily: FONT.medium,
-                                //             fontSize: 14,
-                                //             lineHeight: 21,
-                                //         },
-                                //         right: {
-                                //             color: COLORS.white,
-                                //             fontFamily: FONT.medium,
-                                //             fontSize: 14,
-                                //             lineHeight: 21,
-                                //         },
-                                //     }}
-                                //     wrapperStyle={{
-                                //         left: {
-                                //             backgroundColor: COLORS.secondary,
-                                //             padding: 10,
-                                //             borderRadius: 16
-                                //         },
-                                //         right: {
-                                //             backgroundColor: COLORS.dark,
-                                //             padding: 10,
-                                //             borderRadius: 16
-                                //         },
-                                //     }}
-                                // />
-                            );
-                        }}
-                        renderSend={(props) => (
-                            <View
-                                style={{
-                                    height: 44,
-                                    flexDirection: 'row',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    gap: 14,
-                                    paddingHorizontal: 14,
-                                }}>
-                                {text === '' && (
-                                    <></>        // Later in future will be added a voice and camera buttons
-                                )}
-                                {text !== '' && (
-                                    <Send
-                                        {...props}
-                                        containerStyle={{
-                                            justifyContent: 'center',
-                                        }}>
-                                        <Ionicons name="send" color={COLORS.primary} size={28} />
-                                    </Send>
-                                )}
-                            </View>
-                        )}
-                        renderInputToolbar={renderInputToolbar}
-                        scrollToBottom
-                        scrollToBottomComponent={scrollToBottomComponent}
-                        renderChatFooter={renderChatFooter}
-                    />
-                    {
-                        Platform.OS === 'android' && <KeyboardAvoidingView behavior="padding" />
-                    }
-                </View>
-            </>
-        </SafeAreaView >
+            <View style={{ flex: 1 }}>
+                <GiftedChat
+                    messages={messages}
+                    onSend={onSend}
+                    user={user}
+                    renderMessageText={renderMessageComponent}
+                />
+                <FileSendingButton onFilePicked={handleFilePicked} />
+            </View>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-    composer: {
-        backgroundColor: '#fff',
-        borderRadius: 14,
-        // borderWidth: 1,
-        // borderColor: COLORS.primary,
-        paddingHorizontal: 10,
-        fontSize: 16,
-        // marginVertical: 100
+    fileSendingButton: {
+        backgroundColor: '#333',
+        borderRadius: 10,
+        padding: 10,
+        marginHorizontal: 10,
+        marginBottom: 10,
+        alignSelf: 'flex-end',
+    },
+    fileSendingButtonText: {
+        color: '#fff',
     },
 });
 
