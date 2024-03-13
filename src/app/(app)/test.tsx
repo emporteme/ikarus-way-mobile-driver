26fbfe75639909d9e002d8121992a9e6c75b0877
Magzhan <magzhankarataev02@gmail.com>
2024-03-13 18:12:47 +0600

    public key generation done

--- src/app/(app)/test.tsx
+++ src/app/(app)/test.tsx
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
-import * as Crypto from 'expo-crypto';
-import * as forge from 'node-forge';
-import { ColorSpace } from 'react-native-reanimated';
+import elliptic, { eddsa as EdDSA } from 'elliptic';
+import { hexToUint8Array } from '@/components/core/utils';
+
+const ec = new EdDSA('ed25519');
+
+function toHex(arr: Uint8Array) {
+    return elliptic.utils.toHex(arr).toLowerCase();
+}
+
+function fromHex(hex: string) {
+    return elliptic.utils.toArray(hex.toLowerCase(), 'hex');
+}
 
 export default function App() {
     const [privateKey, setPrivateKey] = useState<string | null>(null);
+    const [publicKey, setPublicKey] = useState<string | null>(null);
     const [location, setLocation] = useState<any>(null);
 
     useEffect(() => {
@@ -26,8 +36,43 @@ export default function App() {
 
     const loadPrivateKey = async () => {
         // Load private key from secure storage
-        const storedPrivateKey = "bb5a9e04a0baaf8cda5cd8718c18d113daa752a4b47dbf10a1c6684a496b241c"; // Replace with your method for loading private key
-        setPrivateKey(storedPrivateKey);
+        const storedPrivateKey = 'bb5a9e04a0baaf8cda5cd8718c18d113daa752a4b47dbf10a1c6684a496b241c'; // Replace with your method for loading private key
+        const storedPublicKey = null; // Replace with your method for loading public key
+
+        // If private and public keys are not stored, generate a new key pair
+        if (!storedPrivateKey || !storedPublicKey) {
+            generateKeyPair();
+        } else {
+            setPrivateKey(storedPrivateKey);
+            setPublicKey(storedPublicKey);
+        }
+    };
+
+    const generateKeyPair = () => {
+        let secret = new Uint8Array(32);
+
+        secret = hexToUint8Array('bb5a9e04a0baaf8cda5cd8718c18d113daa752a4b47dbf10a1c6684a496b241c')
+        /*
+        if (window.crypto && window.crypto.getRandomValues) {
+            secret = new Uint8Array(32);
+            window.crypto.getRandomValues(secret);
+        } else {
+            console.warn('Warning: Using insecure methods to generate private key');
+            secret = [];
+            for (let i = 0; i < 32; i++) {
+                secret.push(Math.random() * 9007199254740991); // aka Number.MAX_SAFE_INTEGER
+            }
+        }
+        */
+
+        const key = ec.keyFromSecret(secret);
+        const privateKeyHex = toHex(key.getSecret());
+        const publicKeyHex = toHex(key.getPublic());
+
+        // Save keys to secure storage
+        // Replace with your method for saving private and public keys
+        setPrivateKey(privateKeyHex);
+        setPublicKey(publicKeyHex);
     };
 
     const signLocation = async () => {
@@ -42,9 +87,6 @@ export default function App() {
             let location = await Location.getCurrentPositionAsync({});
             setLocation(location);
 
-            // Generate public key from private key
-            const publicKey = await getPublicKeyFromPrivateKey(privateKey);
-
             // Constructing the data object
             const dataToSend = {
                 tx: {
@@ -56,19 +98,19 @@ export default function App() {
                         heading: location.coords.heading,
                         latitude: location.coords.latitude,
                         longitude: location.coords.longitude,
-                        speed: location.coords.speed
+                        speed: location.coords.speed,
                     },
                     devpubkey: publicKey,
                     mocked: location.mocked,
                     timestamp: location.timestamp,
                 },
-                type: "AA05"
+                type: 'AA05',
             };
 
             console.log('------------------ DATA BEFORE: ', dataToSend);
 
             // Sign the 'tx' object without pubkey and signature
-            const { signature, orderedData } = await signData(dataToSend.tx, privateKey, publicKey);
+            const { signature, orderedData } = signData(dataToSend.tx, privateKey);
 
             // Update the 'tx' object with the signature and public key
             orderedData.pubkey = publicKey;
@@ -93,9 +135,9 @@ export default function App() {
             const response = await fetch(API_URL, {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(dataToSend)
+                body: JSON.stringify(dataToSend),
             });
 
             console.log('RESPONSE: ', response);
@@ -110,9 +152,38 @@ export default function App() {
         }
     };
 
+    const signData = (data: any, privateKey: string) => {
+        try {
+            // Sort the keys in the data object
+            const orderedData = sortObjectKeys(data);
+
+            // Convert the ordered data object to a string
+            const dataString = JSON.stringify(orderedData);
+
+            // Convert the data string to an array of character codes
+            const messageArray = codifyMessage(dataString);
+
+            // Import private key
+            const key = ec.keyFromSecret(fromHex(privateKey));
+
+            // Sign the message array with the private key
+            const signature = toHex(key.sign(messageArray).toBytes());
+
+            return { signature, orderedData };
+        } catch (error) {
+            console.error('Error signing data:', error);
+            throw error;
+        }
+    };
+
+    const codifyMessage = (message: string) => {
+        return message.split('').map((m) => m.charCodeAt(0));
+    };
+
     return (
         <View style={styles.container}>
             <Text>Private Key: {privateKey}</Text>
+            <Text>Public Key: {publicKey}</Text>
             <Text>Location: {location ? JSON.stringify(location) : 'Not available'}</Text>
         </View>
     );
@@ -123,45 +194,9 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        gap: 50
+        gap: 50,
     },
 });
-
-async function getPublicKeyFromPrivateKey(privateKey: string): Promise<string> {
-    try {
-        // Создаем объект ed25519 из приватного ключа
-        const privateKeyObject = forge.pki.ed25519.makeKeypair({ priv: forge.util.hexToBytes(privateKey) });
-
-        // Получаем публичный ключ в шестнадцатеричном формате
-        const publicKey = forge.util.bytesToHex(privateKeyObject.publicKey);
-
-        return publicKey;
-    } catch (error) {
-        console.error('Error generating public key:', error);
-        throw error;
-    }
-}
-
-async function signData(data: any, privateKey: string, publicKey: string) {
-    try {
-        // Sort the keys in the data object
-        const orderedData = sortObjectKeys(data);
-
-        // Convert the ordered data object to a string
-        const dataString = JSON.stringify(orderedData);
-
-        // Convert the data string to a buffer
-        const buffer = Buffer.from(dataString);
-
-        // Подписываем данные приватным ключом
-        const signature = forge.util.bytesToHex(forge.pki.ed25519.sign({ message: buffer, encoding: 'binary' }, forge.util.hexToBytes(privateKey)));
-
-        return { signature, orderedData };
-    } catch (error) {
-        console.error('Error signing data:', error);
-        throw error;
-    }
-}
 
 const sortObjectKeys = (obj: any): any => {
     if (typeof obj !== 'object' || obj === null) {
@@ -181,25 +216,3 @@ const sortObjectKeys = (obj: any): any => {
 
     return sortedObj as any;
 };
-
-function jsonToUnicodeHex(json: object): string {
-    let jsonString = JSON.stringify(json);
-
-    let unicodeString = '';
-    for (let i = 0; i < jsonString.length; i++) {
-        let charCode = jsonString.charCodeAt(i);
-        if (charCode > 127) {  // Non-Latin characters
-            unicodeString += '\\u' + ('0000' + charCode.toString(16)).slice(-4);
-        } else {
-            unicodeString += jsonString[i];
-        }
-    }
-
-    // Now convert to hex
-    let hexString = '';
-    for (let i = 0; i < unicodeString.length; i++) {
-        hexString += unicodeString.charCodeAt(i).toString(16);
-    }
-
-    return hexString;
-}
