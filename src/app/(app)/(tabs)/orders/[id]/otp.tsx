b975ae5d77e1a0dad4f81f16144cf839bfcada65
Magzhan <magzhankarataev02@gmail.com>
2024-03-16 21:45:42 +0600

    otp

--- src/app/(app)/(tabs)/orders/[id]/otp.tsx
+++ src/app/(app)/(tabs)/orders/[id]/otp.tsx
@@ -1,17 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, SafeAreaView, ScrollView, TextInput, TouchableOpacity, Platform, Button, Image, Pressable, FlatList } from 'react-native';
+import { SafeAreaView, Alert, View, TextInput, TouchableOpacity, Text } from 'react-native';
 import { useLocalSearchParams, Stack, Link, router } from 'expo-router';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { icons } from '@/constants';
-import styles from '@/styles/expenses.style';
-import { OuterDropdown, InnerDropdown } from '@/components';
+import styles from '@/styles/otp.style';
 import { useSession } from '@/components/core/Context';
-import * as ImagePicker from 'expo-image-picker';
-import axios from 'axios';
-import * as FileSystem from 'expo-file-system';
-import * as IntentLauncher from 'expo-intent-launcher';
-import * as DocumentPicker from 'expo-document-picker';
-import Constants from 'expo-constants';
 
 
 const ExpensesPage: React.FC = () => {
@@ -20,12 +11,102 @@ const ExpensesPage: React.FC = () => {
     const { id } = useLocalSearchParams();
     const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
-    const fetchSubmit = async () => {
+    // State
+    const [offerId, setOfferId] = useState('');
+    const [otp, setOTP] = useState('');
+    // const [isPinReady, setIsPinReady] = useState(false);
+    // const maximumCodeLength = 4;
 
+    // Wait offer ID
+    useEffect(() => {
+        askOfferID(jwtToken);
+    }, [jwtToken]);
+
+    const askOfferID = async (jwtToken: string) => {
         try {
+            if (!jwtToken) {
+                throw new Error('JWT token not found');
+            }
+
+            const response = await fetch(`${apiUrl}carrier/orders/offersByOrderCarrierId/${id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + jwtToken
+                },
+            });
+
+            // if (!response.ok) {
+            //     throw new Error('Network response was not ok');
+            // }
+
+            const data = await response.json(); // Parse response data
+            console.log("Offer ID:", data.data[0].id);
+            setOfferId(data.data[0].id); // Update state with fetched data
+        } catch (error) {
+            console.error('Error fetching offer id:', error);
+            alert('Failed to fetch offer id');
+        }
+    }
+
+    // This --> useEffect() when page mounts to send an OTP code request to a client
+    useEffect(() => {
+        askOTP(jwtToken, offerId);
+    }, [jwtToken, offerId]);
+
+    const askOTP = async (jwtToken: string, offerId: string) => {
+        try {
+            if (!jwtToken) {
+                throw new Error('JWT token not found');
+            }
+
+            const response = await fetch(`${apiUrl}carrier/orders/sendOTP/${offerId}?recipientType=SENDER`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + jwtToken
+                },
+            });
+
+            // if (!response.ok) {
+            //     throw new Error('Network response was not ok');
+            // }
+
+            const data = await response.json(); // Parse response data
+            console.log("Asked otp data:", data);
+            // setOTP(data.data); // Update state with fetched data
 
         } catch (error) {
+            console.error('Error asking OTP: ', error);
+            Alert.alert('Error', 'Failed to asking OTP');
+        }
+    }
 
+    const handleOTPSubmit = async () => {
+        try {
+            if (!jwtToken) {
+                throw new Error('JWT token not found');
+            }
+
+            const response = await fetch(`${apiUrl}carrier/orders/startDelivery/${offerId}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + jwtToken
+                },
+                body: JSON.stringify({ otp: otp })
+            });
+
+            // if (!response.ok) {
+            //     throw new Error('Network response was not ok');
+            // }
+
+            const data = await response.json(); // Parse response data
+            console.log("Sent otp data:", data);
+            router.back()
+        } catch (error) {
+            console.error('Error submitting OTP: ', error);
+            Alert.alert('Error', 'Failed to submit OTP. Please try again later.');
         }
     };
 
@@ -34,13 +115,26 @@ const ExpensesPage: React.FC = () => {
             <Stack.Screen
                 options={{
                     headerShown: true,
-                    title: `Order: ${id}`,
+                    title: `OTP code: ${id}`,
                     headerTitleAlign: 'center'
                 }}
             />
-            <ScrollView>
-               
-            </ScrollView>
+            <>
+                <>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Enter OTP code here..."
+                        keyboardType="numeric"
+                        maxLength={6}
+                        value={otp}
+                        onChangeText={setOTP}
+                        placeholderTextColor={'#000'}
+                    />
+                    <TouchableOpacity style={styles.submitButton} onPress={handleOTPSubmit}>
+                        <Text style={styles.submitButtonText}>Send</Text>
+                    </TouchableOpacity>
+                </>
+            </>
         </SafeAreaView>
     )
 }
