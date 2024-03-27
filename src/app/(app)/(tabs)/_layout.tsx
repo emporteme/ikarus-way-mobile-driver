045f654bf50e0779ef61538ce6a3ed40d56330c1
Magzhan <magzhankarataev02@gmail.com>
2024-03-27 12:41:22 +0600

    session logic changed to jwt

--- src/app/(app)/(tabs)/_layout.tsx
+++ src/app/(app)/(tabs)/_layout.tsx
@@ -4,14 +4,14 @@ import { FONT, COLORS, icons } from "@/constants";
 import { useSession } from "@/components/core/AuthContext";
 
 export default function TabsLayout() {
-    const { session, isLoading } = useSession();
+    const { jwtToken } = useSession();
     // You can keep the splash screen open, or render a loading screen like we do here.
-    if (isLoading) {
-        return <Text>Loading...</Text>;
-    }
+    // if (isLoading) {
+    //     return <Text>Loading...</Text>;
+    // }
 
     // Only require authentication within the (app) group's layout as users need to be able to access the (auth) group and sign in again.
-    if (!session) {
+    if (!jwtToken) {
         // On web, static rendering will stop here as the user is not authenticated in the headless Node process that the pages are rendered in.
         return <Redirect href="/auth" />;
     }
